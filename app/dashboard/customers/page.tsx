ddf4115dc29dbec61e109941caa71e58e1553288
Tomjid huda <94817887+TOMJID@users.noreply.github.com>
2025-08-31 17:45:43 +0600

    test

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <p>Customer Page</p>
+    </>
+  );
+}
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <p className="p-10 bg-red-400 tex-4xl">
+        {" "}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui maxime
+        enim id voluptatum, tenetur explicabo nostrum quae, est consectetur
+        veritatis reprehenderit, deserunt commodi ea. Ducimus impedit eius
+        quaerat a tenetur!{" "}
+      </p>
+    </>
+  );
+}
+
+export default page;
