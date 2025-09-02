685544bc68c30d2966d69dbfce702862a634e110
Tomjid huda <94817887+TOMJID@users.noreply.github.com>
2025-09-02 13:49:55 +0600

    backend bug fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,7 +19,6 @@ export default async function Page() {
       <h1 className={`${montserrat.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
@@ -30,12 +29,10 @@ export default async function Page() {
           type="customers"
         />
       </div>
-
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
-
     </main>
   );
 }
