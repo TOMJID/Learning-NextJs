9671dbe961167a85be974746939db5c6d2f95507
Tomjid huda <94817887+TOMJID@users.noreply.github.com>
2025-09-02 13:46:36 +0600

    idk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -15,9 +15,11 @@ export default async function Page() {
   } = await fetchCardData();
   return (
     <main>
+
       <h1 className={`${montserrat.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
+      
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
@@ -28,10 +30,12 @@ export default async function Page() {
           type="customers"
         />
       </div>
+
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
+
     </main>
   );
 }
